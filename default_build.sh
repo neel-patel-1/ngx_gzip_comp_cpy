928181d58a9765c8e0fc209d829f0e88b47edd8d
Neel Patel <n869p538@ittc.ku.edu>
2022-10-10 11:37:12 -0500

    file generation

--- default_build.sh
+++ default_build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+NGINX_VERSION=1.23.1
 
-if [ ! -d "nginx-${NGINX_VERSION}" ]; then
-	wget http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
+if [ ! -d "nginx-${NGINX_VERSION}.tar.gz" ]; then
+	wget --no-check-certificate http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz
 	tar xzf nginx-${NGINX_VERSION}.tar.gz
 fi
 
--- file_gen.sh
+++ file_gen.sh
@@ -3,13 +3,21 @@ f_sz=( "4K" "16K" "32K" )
 for i in "${f_sz[@]}"; do
 	head -c ${i} < /dev/urandom | sudo tee nginx_gzip_build/html/rand_file_${i}.txt >/dev/null
 	gzip -k -f -1  nginx_gzip_build/html/rand_file_${i}.txt
-	size=$( stat nginx_gzip_build/html/rand_file_${i}.txt.gz | grep -Eo 'Size: [0-9]+' | grep -Eo '[0-9]+' )
-	head -c ${size} < /dev/urandom | sudo tee nginx_default_build/html/rand_file_${i}.txt >/dev/null
+	size_1=$( stat nginx_gzip_build/html/rand_file_${i}.txt.gz | grep -Eo 'Size: [0-9]+' | grep -Eo '[0-9]+' )
+	head -c ${size_1} < /dev/urandom | sudo tee nginx_default_build/html/rand_file_${i}_1.txt >/dev/null
+
+	gzip -k -f -9  nginx_gzip_build/html/rand_file_${i}.txt
+	size_9=$( stat nginx_gzip_build/html/rand_file_${i}.txt.gz | grep -Eo 'Size: [0-9]+' | grep -Eo '[0-9]+' )
+	head -c ${size_9} < /dev/urandom | sudo tee nginx_default_build/html/rand_file_${i}_9.txt >/dev/null
 done
 
 for i in "${f_sz[@]}"; do
 	head -c ${i} < /dev/zero | sudo tee nginx_gzip_build/html/zero_file_${i}.txt >/dev/null
 	gzip -k -f -1  nginx_gzip_build/html/zero_file_${i}.txt
-	size=$( stat nginx_gzip_build/html/zero_file_${i}.txt.gz | grep -Eo 'Size: [0-9]+' | grep -Eo '[0-9]+' )
-	head -c ${size} < /dev/zero | sudo tee nginx_default_build/html/zero_file_${i}.txt >/dev/null
+	size_1=$( stat nginx_gzip_build/html/zero_file_${i}.txt.gz | grep -Eo 'Size: [0-9]+' | grep -Eo '[0-9]+' )
+	head -c ${size_1} < /dev/zero | sudo tee nginx_default_build/html/zero_file_${i}_1.txt >/dev/null
+
+	gzip -k -f -9  nginx_gzip_build/html/zero_file_${i}.txt
+	size_9=$( stat nginx_gzip_build/html/zero_file_${i}.txt.gz | grep -Eo 'Size: [0-9]+' | grep -Eo '[0-9]+' )
+	head -c ${size_9} < /dev/zero | sudo tee nginx_default_build/html/zero_file_${i}_9.txt >/dev/null
 done

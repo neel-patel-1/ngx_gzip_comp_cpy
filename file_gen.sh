d3729b23ae7e9298558d5f43f76df9ebcea17a23
Neel Patel <n869p538@ittc.ku.edu>
2022-10-19 13:55:18 -0500

    file gen

--- file_gen.sh
+++ file_gen.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+wget --no-check-certificate https://sun.aei.polsl.pl//~sdeor/corpus/mr.bz2
+bzip2 -dfk mr.bz2
+
 f_sz=( "4K" "16K" "32K" )
 for i in "${f_sz[@]}"; do
-	head -c ${i} < /dev/urandom | sudo tee nginx_gzip_build/html/rand_file_${i}.txt >/dev/null
-	gzip -k -f -1  nginx_gzip_build/html/rand_file_${i}.txt
+	head -c ${i} < /dev/urandom | sudo tee nginx_default_build/html/rand_file_${i}.txt >/dev/null
+	gzip -k -f -1  nginx_default_build/html/rand_file_${i}.txt
 	size_1=$( stat nginx_gzip_build/html/rand_file_${i}.txt.gz | grep -Eo 'Size: [0-9]+' | grep -Eo '[0-9]+' )
-	head -c ${size_1} < /dev/urandom | sudo tee nginx_default_build/html/rand_file_${i}_1.txt >/dev/null
+	head -c ${size_1} < /dev/urandom | sudo tee nginx_gzip_build/html/rand_file_${i}_1.txt >/dev/null
 
 	gzip -k -f -9  nginx_gzip_build/html/rand_file_${i}.txt
 	size_9=$( stat nginx_gzip_build/html/rand_file_${i}.txt.gz | grep -Eo 'Size: [0-9]+' | grep -Eo '[0-9]+' )
-	head -c ${size_9} < /dev/urandom | sudo tee nginx_default_build/html/rand_file_${i}_9.txt >/dev/null
+	head -c ${size_9} < /dev/urandom | sudo tee nginx_gzip_build/html/rand_file_${i}_9.txt >/dev/null
 done
 
 for i in "${f_sz[@]}"; do
@@ -19,5 +22,8 @@ for i in "${f_sz[@]}"; do
 
 	gzip -k -f -9  nginx_gzip_build/html/zero_file_${i}.txt
 	size_9=$( stat nginx_gzip_build/html/zero_file_${i}.txt.gz | grep -Eo 'Size: [0-9]+' | grep -Eo '[0-9]+' )
-	head -c ${size_9} < /dev/zero | sudo tee nginx_default_build/html/zero_file_${i}_9.txt >/dev/null
+	head -c ${size_9} < /dev/zero | sudo tee nginx_gzip_build/html/zero_file_${i}_9.txt >/dev/null
 done
+
+for i in "${corpus[@]}"; do
+	head -c ${i} < $i | sudo tee nginx_gzip_build
